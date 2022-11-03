8782a68dd0a2b258d0d009493a0b7596f6edc2b3
Hans Yadav <hansy@users.noreply.github.com>
2022-11-03 02:29:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const Queue = require("bull");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || "5000";
+const REDIS_URL = process.env.REDISCLOUD_URL || "redis://127.0.0.1:6379";
+
+const app = express();
+app.use(bodyParser.json());
+const workQueue = new Queue("testStream", REDIS_URL);
+
+app.get("/", (req, res) => res.sendFile("index.html", { root: __dirname }));
+
+app.post("/job", async (req, res) => {
+  const { streamKey } = req.body;
+  const job = await workQueue.getJob(streamKey);
+
+  if (job) {
+    const progress = job._progress;
+
+    if (progress !== "running") {
+      await job.remove();
+    } else {
+      return res.json({ status: "running" });
+    }
+  }
+
+  await workQueue.add({ streamKey }, { jobId: streamKey });
+
+  res.json({ status: "started" });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
---
+++ worker.js
@@ -0,0 +1,66 @@
+require("dotenv").config();
+const throng = require("throng");
+const Queue = require("bull");
+const { spawn } = require("child_process");
+
+const REDIS_URL = process.env.REDISCLOUD_URL || "redis://127.0.0.1:6379";
+const workers = process.env.WEB_CONCURRENCY || 1;
+const maxJobsPerWorker = 2;
+
+const start = () => {
+  const workQueue = new Queue("testStream", REDIS_URL);
+
+  workQueue.process(maxJobsPerWorker, async (job) => {
+    const { streamKey } = job.data;
+    const RTMP_INGEST = `rtmp://rtmp.livepeer.com/live/${streamKey}`;
+    job.progress("running");
+    const stream = spawn("ffmpeg", [
+      "-re",
+      "-i",
+      "puppy_timer.mp4",
+      "-c:v",
+      "libx264",
+      "-preset",
+      "veryfast",
+      "-b:v",
+      "3000k",
+      "-maxrate",
+      "3000k",
+      "-bufsize",
+      "6000k",
+      "-g",
+      "50",
+      "-c:a",
+      "aac",
+      "-b:a",
+      "160k",
+      "-ac",
+      "2",
+      "-ar",
+      "44100",
+      "-f",
+      "flv",
+      RTMP_INGEST,
+    ])
+      .on("error", () => {
+        job.progress("error");
+        process.exit(1);
+      })
+      .on("close", () => {
+        job.progress("complete");
+        process.exit(0);
+      });
+
+    stream.stdout.on("data", (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    stream.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    return true;
+  });
+};
+
+throng({ workers, start });
