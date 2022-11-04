6c7fa966f70e3b25838544f85c53f2e2e3b26416
Hans Yadav <hansy@users.noreply.github.com>
2022-11-04 00:50:52 -0400

    remove background worker logic

--- server.js
+++ server.js
@@ -1,26 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-const Queue = require("bull");
 const { spawn } = require("child_process");
-const { createBullBoard } = require("@bull-board/api");
-const { BullAdapter } = require("@bull-board/api/bullAdapter");
-const { ExpressAdapter } = require("@bull-board/express");
 const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || "5000";
-const REDIS_URL = process.env.REDISCLOUD_URL || "redis://127.0.0.1:6379";
 
 const app = express();
 app.use(bodyParser.json());
-const workQueue = new Queue("testStream", REDIS_URL);
-const serverAdapter = new ExpressAdapter();
-
-serverAdapter.setBasePath("/admin/queues");
-
-createBullBoard({
-  queues: [new BullAdapter(workQueue)],
-  serverAdapter: serverAdapter,
-});
 
 const RUNNING_STREAMS = {};
 
@@ -56,8 +42,6 @@ const spawnFFMPEGStream = (streamKey) => {
   });
 };
 
-app.use("/admin/queues", serverAdapter.getRouter());
-
 app.get("/", (req, res) => res.sendFile("index.html", { root: __dirname }));
 
 app.post("/stream", async (req, res) => {
@@ -70,27 +54,6 @@ app.post("/stream", async (req, res) => {
 
   RUNNING_STREAMS[streamKey] = "running";
   spawnFFMPEGStream(streamKey);
-
-  // const job = await workQueue.getJob(streamKey);
-
-  // if (job) {
-  //   const progress = job._progress;
-
-  //   if (progress === "bad key") {
-  //     return res.json({ status: "bad key" });
-  //   }
-
-  //   if (progress !== "running") {
-  //     console.log("Old job found; removing...");
-  //     await job.remove();
-  //   } else {
-  //     console.log("Job already running");
-  //     return res.json({ status: "running" });
-  //   }
-  // }
-
-  // console.log("Adding new job to queue");
-  // await workQueue.add({ streamKey }, { jobId: streamKey });
 
   res.json({ status: "started" });
 });
